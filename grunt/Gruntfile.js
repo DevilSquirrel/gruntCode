9fd43833152fa3e2c8044e0b97bcdbd3077a6317
liuyuchuan <18611448841@163.com>
2023-10-08 18:25:49 +0800

    less

--- grunt/Gruntfile.js
+++ grunt/Gruntfile.js
@@ -1,7 +1,16 @@
 module.exports = function (grunt) {
   grunt.initConfig({
-    jshint: ['Gruntfile.js']
+    jshint: ['Gruntfile.js'],
+    less:{
+      compile:{
+        files: {
+          'build/css/compiled.css': 'public/css/layout.less'
+        }
+      }
+    }
   });
+
+  grunt.loadNpmTasks('grunt-contrib-less');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.registerTask('default',['jshint']);
 };
